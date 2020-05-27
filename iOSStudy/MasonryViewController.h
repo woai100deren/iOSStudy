c0e70e76ef8e76397c96f8a2d60a5fcd1e550a3e
wangjing <wangjing@dodoca.com>
2020-05-27 13:32:59 +0800

    uitableview

---
+++ iOSStudy/MasonryViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasonryViewController.h
+//  iOSStudy
+//
+//  Created by dodoca on 2020/5/27.
+//  Copyright © 2020 dodoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MasonryViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSStudy/MasonryViewController.m
@@ -0,0 +1,32 @@
+//
+//  MasonryViewController.m
+//  iOSStudy
+//
+//  Created by dodoca on 2020/5/27.
+//  Copyright © 2020 dodoca. All rights reserved.
+//
+
+#import "MasonryViewController.h"
+
+@interface MasonryViewController ()
+
+@end
+
+@implementation MasonryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
