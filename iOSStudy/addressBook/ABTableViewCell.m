6cee1ba68cfeb5bcdda500a6a3c0f9fd48cded28
wangjing <wangjing@dodoca.com>
2020-06-04 09:52:52 +0800

    联系人列表展示

---
+++ iOSStudy/addressBook/ABTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ABTableViewCell.m
+//  iOSStudy
+//
+//  Created by dodoca on 2020/6/4.
+//  Copyright © 2020 dodoca. All rights reserved.
+//
+
+#import "ABTableViewCell.h"
+
+@implementation ABTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
