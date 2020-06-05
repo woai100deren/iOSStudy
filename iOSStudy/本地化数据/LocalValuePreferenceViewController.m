73d0fdb9ce84e85d6402c434a678ecf0904d2f27
wangjing <wangjing@dodoca.com>
2020-06-05 09:42:13 +0800

    Preference方式存取数据实现

--- iOSStudy/本地化数据/LocalValuePreferenceViewController.m
+++ iOSStudy/本地化数据/LocalValuePreferenceViewController.m
@@ -17,6 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"Preference方式";
+    
+    [self saveData];
+}
+
+-(void)saveData{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setURL:[NSURL URLWithString:@"https://www.baidu.com"] forKey:@"url"];
+    [defaults setObject:@"这是一串字符串" forKey:@"object"];
+    [defaults setBool:YES forKey:@"bool"];
+    [defaults setFloat:10.6 forKey:@"float"];
+    
+    //立即写入到文件中
+    [defaults synchronize];
+    
+    //读取
+    NSLog(@"getUrl:%@",[defaults URLForKey:@"url"]);
+    NSLog(@"getObject:%@",[defaults objectForKey:@"object"]);
+    NSLog(@"getBool:%i",[defaults boolForKey:@"bool"]);
+    NSLog(@"getFloat:%f",[defaults floatForKey:@"float"]);
 }
 
 @end
